b46c95005221f0f164dd8de7fba1b2c19ecfc0d6
Mauricio Paternina <spaceinvadev@icloud.com>
2022-02-14 14:59:51 -0500

    feature: /playlists page

--- client/src/pages/playlists.js
+++ client/src/pages/playlists.js
@@ -1,1 +1,28 @@
-export const Playlists = () => <h1>Playlists</h1>
+import { useEffect, useState } from 'react'
+
+import { getCurrentUserPlaylists } from '../spotify'
+import { catchErrors } from '../utils'
+
+import { PlaylistsGrid, Section } from '../components'
+
+export const Playlists = () => {
+  const [playlists, setPlaylists] = useState(null)
+
+  useEffect(() => {
+    const fetchPlaylistsData = async () => {
+      const { data: playlistsData } = await getCurrentUserPlaylists()
+
+      setPlaylists(playlistsData)
+    }
+
+    catchErrors(fetchPlaylistsData())
+  }, [])
+
+  return (
+    <Section title="Top playlists" breadcrumb={true}>
+      {playlists && playlists.items && (
+        <PlaylistsGrid playlists={playlists.items} />
+      )}
+    </Section>
+  )
+}
